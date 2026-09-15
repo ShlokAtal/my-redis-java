f6add5480a3cd41adab40828aa90b1123de9ea80
Shlok Atal <shlokatal78@gmail.com>
2026-09-15 21:48:25 +0530

    feat: implement basic Redis TCP server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,20 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(6379);
+        System.out.println("Redis server started on port 6379");
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Client connected");
+
+        OutputStream output = clientSocket.getOutputStream();
+        output.write("+PONG\r\n".getBytes());
+        output.flush();
+        clientSocket.getInputStream().read();
+    }
 }
