fea8f78d214bd9272deccca88fc74a72950f7823
Shlok Atal <shlokatal78@gmail.com>
2026-09-18 10:54:45 +0530

    feat: implement ECHO command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,17 +12,60 @@ public class Main
 
         ServerSocket serverSocket = new ServerSocket(6379);
         System.out.println("Redis server started on port 6379");
+
         Socket clientSocket = serverSocket.accept();
         System.out.println("Client connected");
 
         InputStream input = clientSocket.getInputStream();
         OutputStream output = clientSocket.getOutputStream();
+
         byte[] buffer = new byte[1024];
-        int bytesRead = input.read(buffer);
-        String command = new String(buffer, 0, bytesRead);
-        System.out.println("Received: " + command);
-        output.write("+PONG\r\n".getBytes());
-        output.flush();
-        clientSocket.getInputStream().read();
+
+        while (true) 
+        {
+            int bytesRead = input.read(buffer);
+
+            if (bytesRead == -1) 
+            {
+                break;
+            }
+
+            String request = new String(buffer, 0, bytesRead);
+            String[] parts = request.split("\r\n");
+
+            int argumentCount = Integer.parseInt(parts[0].substring(1));
+            int argumentLength = Integer.parseInt(parts[1].substring(1));
+
+            String command = parts[2];
+
+            String message = null;
+
+            if (argumentCount == 2) 
+            {
+                int messageLength = Integer.parseInt(parts[3].substring(1));
+                message = parts[4];
+
+                System.out.println("Message length: " + messageLength);
+                System.out.println("Message: " + message);
+            }
+
+            System.out.println("Arguments: " + argumentCount);
+            System.out.println("Command length: " + argumentLength);
+            System.out.println("Command: " + command);
+
+            if (command.length() == argumentLength && command.equals("PING")) 
+            {
+                output.write("+PONG\r\n".getBytes());
+            } 
+            else if (command.length() == argumentLength && command.equals("ECHO") && message != null) 
+            {
+                output.write(("$" + message.length() + "\r\n" + message + "\r\n").getBytes());
+            } 
+            else 
+            {
+                output.write("-ERR unknown command\r\n".getBytes());
+            }
+            output.flush();
+        }
     }
 }
